611e4fc1531efa75a465300e281596d9b2b59c99
Iuri Johansson <iuri_abreu@hotmail.com>
2022-10-26 17:55:32 +0000

    Add basic struture to battleships game and start coding the board

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# creating a board
+board = []
+for x in range(5):
+    board.append([" "] * 5)
+
+def print_board(board):
+    for row in board:
+        print(row)
+
+print_board(board)
