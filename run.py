6cf9083d666443b5a24c07aa350c3b2cd6f0904b
Iuri Johansson <iuri_abreu@hotmail.com>
2022-10-26 18:01:01 +0000

    Add a function to create randomly ships on the board

--- run.py
+++ run.py
@@ -3,12 +3,23 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 # creating a board
-board = []
-for x in range(5):
-    board.append([" "] * 5)
+from random import randint
+
+computer_board = [[" "]*6 for x in range(6)]
+player_board = [[" "]*6 for x in range(6)]
 
 def print_board(board):
-    for row in board:
-        print(row)
 
-print_board(board)
+
+# create ships
+def create_ships()
+    for ship in range(5):
+        ship_row, ship_column = randint(0, 6), randint(0, 6)
+        while board[ship_row][ship_column] == "X"
+            ship_row, ship_column = randint(0, 6), randint(0, 6)
+
+# positioning ships on board
+def pos_ships():
+
+# counting how many ships were hit
+def hit_ships():
