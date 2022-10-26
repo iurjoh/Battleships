e76ce1e5f326f56c5940fc004e8605306a1552c6
Iuri Johansson <iuri_abreu@hotmail.com>
2022-10-26 18:33:27 +0000

    Add instructions and user name functions

--- run.py
+++ run.py
@@ -2,9 +2,18 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-# creating a board
+
 from random import randint
 
+"""
+    starting the game and giving instructions
+"""
+print("___________________________________")
+print("Welcome to BATTLESHIPS GAME!")
+print("Board size: 5. Number of ships: 4.")
+print("___________________________________")
+
+# creating a board
 computer_board = [[" "]*6 for x in range(6)]
 player_board = [[" "]*6 for x in range(6)]
 
@@ -13,10 +22,16 @@ def print_board(board):
 
 # create ships
 def create_ships()
+    """
+    Creating ships on the board suing a random position
+    """
+
     for ship in range(5):
         ship_row, ship_column = randint(0, 6), randint(0, 6)
-        while board[ship_row][ship_column] == "X"
+        while board[ship_row][ship_column] == "X":
             ship_row, ship_column = randint(0, 6), randint(0, 6)
+        board[ship_row][ship_column] = "X"
+
 
 # positioning ships on board
 def pos_ships():
