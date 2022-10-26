7793672d356ac9a02943e9f6ce932b8853f2d958
Iuri Johansson <iuri_abreu@hotmail.com>
2022-10-26 18:40:45 +0000

    fixing problems to print instructions and user name

--- run.py
+++ run.py
@@ -5,36 +5,46 @@
 
 from random import randint
 
-"""
-    starting the game and giving instructions
-"""
-print("___________________________________")
+def instructions():
+    """
+    starting with some instructions
+    """
+print("-----------------------------------")
 print("Welcome to BATTLESHIPS GAME!")
 print("Board size: 5. Number of ships: 4.")
-print("___________________________________")
-
-# creating a board
-computer_board = [[" "]*6 for x in range(6)]
-player_board = [[" "]*6 for x in range(6)]
-
-def print_board(board):
-
-
-# create ships
-def create_ships()
+print("-----------------------------------")
+instructions()
+# creating a user name
+def user_name():
     """
-    Creating ships on the board suing a random position
+    Creating a user name 
     """
-
-    for ship in range(5):
-        ship_row, ship_column = randint(0, 6), randint(0, 6)
-        while board[ship_row][ship_column] == "X":
-            ship_row, ship_column = randint(0, 6), randint(0, 6)
-        board[ship_row][ship_column] = "X"
+    user_name_string = input ("Please commander, enter your name here: ")
+    print(f"Welcome Commander {user_name_string}")
+user_name()
 
 
-# positioning ships on board
-def pos_ships():
+# # creating a board
+# computer_board = [[" "]*6 for x in range(6)]
+# player_board = [[" "]*6 for x in range(6)]
 
-# counting how many ships were hit
-def hit_ships():
+# def print_board(board):
+
+
+# # create ships
+# def create_ships()
+#     """
+#     Creating ships on the board suing a random position
+#     """
+#     for ship in range(5):
+#         ship_row, ship_column = randint(0, 6), randint(0, 6)
+#         while board[ship_row][ship_column] == "X":
+#             ship_row, ship_column = randint(0, 6), randint(0, 6)
+#         board[ship_row][ship_column] = "X"
+
+
+# # positioning ships on board
+# def pos_ships():
+
+# # counting how many ships were hit
+# def hit_ships():
