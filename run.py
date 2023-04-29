5f4504ba517de5bba584ca6471de8c80227d699d
Iuri Johansson <iuri_abreu@hotmail.com>
2023-04-29 11:58:22 +0000

    Updating the game with better logic, new options, code refactored

--- run.py
+++ run.py
@@ -2,113 +2,190 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-
+import sys
 from random import randint
-
-board = []
-
-
-def instructions():
-    """
-    starting with some instructions
-    """
-print("------------------------------------------")
-print("         Welcome to TREASURE HUNT!        ")
-print("Board size: 5x5. Treasure: 1. Chances: 10.")
-print("------------------------------------------")
-instructions()
-
-
-def user_name():
-    """
-    Creating a user name
-    """
-    user_name_string = input ("Please explorer, enter your name here: ")
-    print(f"Welcome explorer {user_name_string},")
-    print("There is a chest full of treasures, but it's hidden around here.")
-    print("You have only 10 chances to find your treasure.")
-    print("Choose a place to dig with intelligence and precision.")
-    print(f"Good luck, explorer {user_name_string}! \n")
-
-user_name()
 
 
 def print_board(board):
     """
-    Creating a 2d board to the game
+    Creates a 2d board to the game
     """
-    print("    0 1 2 3 4  ")
-    print("  +-----------+")
-    for row in range(5):
-        board.append(["-"] * 5)
-    number = 0
-    for row in range(5):
-        print((number), end= " | ")
-        for column in range(len(board[number])):
-            print(board[number][column], end= " ")
-        print("| ")
-        number += 1
-    print("  +-----------+")
-print_board(board)
+    print("   ", end="")
+    for col in range(len(board)):
+        print(col, end=" ")
+    print()
+
+    for row in range(len(board)):
+        print(row, end="  ")
+        for col in range(len(board[row])):
+            print(board[row][col], end=" ")
+        print()
 
 
-def random_row(board):
+def setup_board(size):
     """
-    Randomly placing a chest in a row
+    Initializes a game board of a given size.
     """
-    return randint(0, 4)
+    board = []
+    for _ in range(size):
+        board.append(["-"] * size)
+    return board
 
-def random_col(board):
+
+def place_chest(board):
     """
-    Randomly placing a chest in a column
+    Randomly places the chest in the game board.
     """
-    return randint(0, 4)
-    
-chest_row = random_row(board)
-chest_col = random_col(board)
+    chest_row = randint(0, len(board) - 1)
+    chest_col = randint(0, len(board) - 1)
+    return chest_row, chest_col
 
-# used to test the game during coding
-# print(chest_row)
-# print(chest_col)
 
-guesses = 0
-
-while guesses < 10:
-    print("Explorer, where should we dig now?")
+def get_valid_board_size():
     """
-    Input for the user's guess
+    Asks the player for a valid board size.
     """
-    guess_row = int(input ("Choose a row:"))
-    guess_col = int(input ("Choose a column:"))
+    while True:
+        size_str = input("Enter the board size (3, 5, or 9): ")
+        if size_str.isdigit():
+            size = int(size_str)
+            if size in [3, 5, 9]:
+                return size
+        print("Invalid input. Please enter a valid board size (3, 5, or 9).")
 
 
-    # If the chest was discovered
-    if guess_row == chest_row and guess_col == chest_col:
-        print("'History may be accurate. But archaeology is precise.' - Doug Scott")
-        print("Congratulations, you finally found the treasure!")
-
-        # After 10 guesses
-        break
-
-    # If the shoot are outside the battlefield
-    elif guess_row not in range(5) or guess_col not in range(5):
-        print("Sorry, these coordinates are outside of the map.")
+def reveal_chest_location(board, chest_row, chest_col):
+    """
+    Reveals the location of the chest on the board.
+    """
+    board[chest_row][chest_col] = "T"
 
 
-    # If was already choosen
-    elif board[guess_row][guess_col] == "X":
-        print("Sorry, this coordinate has already been assigned.")
+def play_game(name, size):
+    """
+    Runs the treasure hunt game of a given size.
+    """
+    board = setup_board(size)
+    chest_row, chest_col = place_chest(board)
 
+    # print chest location for testing purposes
+    print(f"Chest located at ({chest_row}, {chest_col})")
 
-    # If you missed the target
-    else:
-        print("Ops, there is no treasure here. Try again.")
-        board[guess_row][guess_col] = "X"
+    max_guesses = int(size * 1.5)
+
+    print(
+        f"Welcome to the TREASURE HUNT! Board size: {size}x{size}." 
+        f" Treasure: 1. Guesses: {max_guesses}.")
+
+    for guess_num in range(1, max_guesses + 1):
+        print(
+            f"Explorer {name}, where should we dig now?"
+            f" Guess {guess_num} of {max_guesses}.")
         print_board(board)
-        
-        # Removing a user's chance
-        guesses += 1
-        print("You already used", guesses, "of 10 guesses.")
 
-        if guesses == 10:
-            print("Sorry great explorer, but this time you didn't find the treasure!")
+        valid_input = False
+        while not valid_input:
+            guess_row_str = input("Choose a row: ")
+            guess_col_str = input("Choose a column: ")
+
+            if not guess_row_str.isdigit() or not guess_col_str.isdigit():
+                print(
+                    f"Invalid option, explorer {name}."
+                    f" Please enter valid numbers between 0 and {size - 1}.")
+                continue
+
+            guess_row = int(guess_row_str)
+            guess_col = int(guess_col_str)
+
+            if guess_row not in range(size) or guess_col not in range(size):
+                print(f"Sorry explorer {name}, those coordinates are outside the map.")
+                continue
+
+            if board[guess_row][guess_col] == "X":
+                print(
+                    f"This location was explored."
+                    f" Please, try a new one explorer {name}.")
+                continue
+
+            valid_input = True
+
+        if guess_row == chest_row and guess_col == chest_col:
+            # mark treasure location on board
+            board[guess_row][guess_col] = "T"
+            print_board(board)
+            print(
+                f'"History may be accurate. But archaeology is precise." - Doug Scott.'
+                f' Congratulations, explorer {name}! You found the treasure!')
+            restart_game(name, size)
+            return
+
+        print(f"Sorry, explorer {name}. You missed the treasure!")
+        board[guess_row][guess_col] = "X"
+
+    reveal_chest_location(board, chest_row, chest_col)
+    print_board(board)
+    print(
+        f"Sorry, explorer {name}. You didn't find the treasure this time."
+        f"The location of the treasure was ({chest_row}, {chest_col}).")
+    restart_game(name, size)
+
+
+def restart_game(name, size):
+    """
+    Asks the player if they want to start a new game, and prompts for level choice if they choose to play again.
+    """
+    valid_input = False
+    while not valid_input:
+        answer = input(f"Explorer {name}, would you like to start a new Treasure Hunt adventure? (Y/N): ")
+        if answer.upper() == "Y":
+            print("Please choose your difficulty level:")
+            print("1. Easy (board size: 3x3)")
+            print("2. Medium (board size: 5x5)")
+            print("3. Hard (board size: 9x9)")
+            while True:
+                level_choice = input("Enter your choice (1-3): ")
+                if level_choice == "1":
+                    play_game(name, 3)
+                    break
+                elif level_choice == "2":
+                    play_game(name, 5)
+                    break
+                elif level_choice == "3":
+                    play_game(name, 9)
+                    break
+                else:
+                    print("Invalid choice. Please enter a number between 1 and 3.")
+            valid_input = True
+        elif answer.upper() == "N":
+            print("Thanks for playing! Come back soon!")
+            sys.exit()  # exit the program
+        else:
+            print("Invalid input. Please enter Y or N.")
+
+
+# Start the game
+print("Welcome to Treasure Hunt! Please explorer, enter your name:")
+name = input()
+
+
+while True:
+    print("Please, choose your difficulty level:")
+    print("1. Easy (board size: 3x3)")
+    print("2. Medium (board size: 5x5)")
+    print("3. Hard (board size: 9x9)")
+
+    level_choice = input("Enter your choice (1-3): ")
+    if level_choice == "1":
+        play_game(name, 3)
+        restart_game(name, 3)
+        break
+    elif level_choice == "2":
+        play_game(name, 5)
+        restart_game(name, 5)
+        break
+    elif level_choice == "3":
+        play_game(name, 9)
+        restart_game(name, 9)
+        break
+    else:
+        print("Invalid choice. Please enter a number between 1 and 3.")
